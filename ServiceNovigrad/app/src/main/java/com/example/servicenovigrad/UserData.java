62b076cb7868901b15e999a97395f0977994c2f6
notanthony <60042005+notanthony@users.noreply.github.com>
2020-10-06 18:43:33 -0400

    Implemented Parcelable to UserData

--- ServiceNovigrad/app/src/main/java/com/example/servicenovigrad/UserData.java
+++ ServiceNovigrad/app/src/main/java/com/example/servicenovigrad/UserData.java
@@ -1,17 +1,53 @@
 package com.example.servicenovigrad;
 
-public class UserData {
-    public String name,email,role;
+import android.os.Parcel;
+import android.os.Parcelable;
 
-    public UserData()
-    {
+public class UserData implements Parcelable {
 
+    public static final Parcelable.Creator CREATOR = new Parcelable.Creator() {
+        public User createFromParcel(Parcel in) {
+            return new UserData(in);
+        }
+
+        public UserData[] newArray(int size) {
+            return new UserData[size];
+        }
+    };
+
+    private String name;
+    private String role;
+
+    // Constructor
+    public UserData(String name, String role){
+        this.name = name;
+        this.role = role;
     }
-    public UserData(String name,String email, String role) {
-        name = name;
-        email = email;
-        role = role;
 
+    public String getName() {
+        return name;
     }
+    
+    public String getRole() {
+        return role;
+    }
+
+    // Parcelling part
+      public UserData(Parcel in){
+           this.name = in.readString();
+           this.role =  in.readString();
+       }
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+       public void writeToParcel(Parcel dest, int flags) {
+        dest.writeString(this.name);
+        dest.writeString(this.role);
+       }
 }
+
 

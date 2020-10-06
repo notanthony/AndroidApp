31be469d7a23002cfa1efbe368cf9f4fdd93cac7
Siavash-Khalaj <skhal045@uottawa.ca>
2020-10-05 23:48:41 -0400

    Added UserData.java

---
+++ ServiceNovigrad/app/src/main/java/com/example/servicenovigrad/UserData.java
@@ -0,0 +1,17 @@
+package com.example.servicenovigrad;
+
+public class UserData {
+    public String name,email,role;
+
+    public UserData()
+    {
+
+    }
+    public UserData(String name,String email, String role) {
+        name = name;
+        email = email;
+        role = role;
+
+    }
+}
+

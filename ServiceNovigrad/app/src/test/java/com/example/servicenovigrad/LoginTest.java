0e6dd1cb0ff4f70ee2fe316cbd0ee9ca38c12be1
Austin Fan <afan062@uottawa.ca>
2020-10-06 23:25:03 -0400

    added LoginTest.java and updated RegisterTest.java

---
+++ ServiceNovigrad/app/src/test/java/com/example/servicenovigrad/LoginTest.java
@@ -0,0 +1,12 @@
+package com.example.servicenovigrad;
+
+import junit.framework.TestCase;
+
+public class LoginTest extends TestCase {
+
+    public void testOnCreate() {
+    }
+
+    public void testOnLoginButtonClicked() {
+    }
+}
--- ServiceNovigrad/app/src/test/java/com/example/servicenovigrad/RegisterTest.java
+++ ServiceNovigrad/app/src/test/java/com/example/servicenovigrad/RegisterTest.java
@@ -2,6 +2,9 @@ package com.example.servicenovigrad;
 
 import junit.framework.TestCase;
 
+import org.junit.Assert;
+import org.junit.Test;
+
 import static org.junit.Assert.assertTrue;
 
 public class RegisterTest extends TestCase {
@@ -15,12 +18,17 @@ public class RegisterTest extends TestCase {
     public void testOnEmployeeButtonClicked() {
     }
 
+    @Test
     public void testOnRegisterButtonClicked() {
 
         Register register = new Register();
 
         String inputPassword = "adminadmin";
         String inputPassword2 = "adminadmin";
-        assertEquals(inputPassword,inputPassword2);
+        assertFalse(inputPassword.isEmpty()); //testing if a password is entered
+        assertTrue(!inputPassword.isEmpty()); //another test to see a if password is entered
+        Assert.assertTrue("password is at least 6 characters", inputPassword.length()>=6);
+        assertEquals(inputPassword,inputPassword2); //testing if passwords match
+
     }
 }

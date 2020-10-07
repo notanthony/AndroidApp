cc8a394b74d1a0b45781f6d66463a3e314ee1de6
notanthony <60042005+notanthony@users.noreply.github.com>
2020-10-06 21:17:49 -0400

    Update MainActivity.java

--- ServiceNovigrad/app/src/main/java/com/example/servicenovigrad/MainActivity.java
+++ ServiceNovigrad/app/src/main/java/com/example/servicenovigrad/MainActivity.java
@@ -6,7 +6,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import import android.view.View.OnClickListener;
+import android.view.View.OnClickListener;
 
 import com.google.firebase.auth.FirebaseAuth;
 

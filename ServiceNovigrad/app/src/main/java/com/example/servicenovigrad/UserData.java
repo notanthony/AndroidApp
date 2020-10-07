dcf36ca7f5efc557963b68c2b052554bf619d461
anthony <anthony.andy.zhao@gmail.com>
2020-10-06 21:24:36 -0400

    Changed UserData

--- ServiceNovigrad/app/src/main/java/com/example/servicenovigrad/UserData.java
+++ ServiceNovigrad/app/src/main/java/com/example/servicenovigrad/UserData.java
@@ -3,17 +3,7 @@ package com.example.servicenovigrad;
 import android.os.Parcel;
 import android.os.Parcelable;
 
-public class UserData implements Parcelable {
-
-    public static final Parcelable.Creator CREATOR = new Parcelable.Creator() {
-        public UserData createFromParcel(Parcel in) {
-            return new UserData(in);
-        }
-
-        public UserData[] newArray(int size) {
-            return new UserData[size];
-        }
-    };
+public class UserData {
 
     private String name;
     private String role;
@@ -32,22 +22,6 @@ public class UserData implements Parcelable {
         return role;
     }
 
-    // Parcelling part
-      public UserData(Parcel in){
-           this.name = in.readString();
-           this.role =  in.readString();
-       }
-
-    @Override
-    public int describeContents() {
-        return 0;
-    }
-
-    @Override
-       public void writeToParcel(Parcel dest, int flags) {
-        dest.writeString(this.name);
-        dest.writeString(this.role);
-       }
 }
 
 

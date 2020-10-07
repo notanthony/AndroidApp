4b6f01462e81a3f2eb13bdea377fb85f2c73eacb
notanthony <60042005+notanthony@users.noreply.github.com>
2020-10-07 17:06:57 -0400

    Made admin a subclass of user

--- ServiceNovigrad/app/src/main/java/com/example/servicenovigrad/Admin.java
+++ ServiceNovigrad/app/src/main/java/com/example/servicenovigrad/Admin.java
@@ -1,33 +1,7 @@
-package com.example.servicenovigrad;
-
-import androidx.appcompat.app.AppCompatActivity;
-
-import android.content.Intent;
-import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
-import android.widget.TextView;
-
-import com.google.firebase.auth.FirebaseAuth;
-
-public class Admin extends AppCompatActivity {
-    FirebaseAuth fAuth;
-    Button logout;
-    TextView userType;
-
-    @Override
+public class Admin extends User {
+  
     protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_admin);
-        logout = findViewById(R.id.logout);
-        fAuth = FirebaseAuth.getInstance();
-        userType = (TextView) findViewById(R.id.welcomeUser);
-        userType.setText("Welcome Admin");
-    }
-    public void onLogoutButtonClicked(View view){
-        fAuth.signOut();
-        Intent intent = new Intent (Admin.this, MainActivity.class);
-        startActivity(intent);
-        finish();
+        super.onCreate(savedInstanceState);
     }
 }

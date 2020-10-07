be89add2188d3e14f65b1829493a92ec7a70fd0f
notanthony <60042005+notanthony@users.noreply.github.com>
2020-10-06 21:17:36 -0400

    Update MainActivity.java

--- ServiceNovigrad/app/src/main/java/com/example/servicenovigrad/MainActivity.java
+++ ServiceNovigrad/app/src/main/java/com/example/servicenovigrad/MainActivity.java
@@ -6,10 +6,13 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import import android.view.View.OnClickListener;
+
+import com.google.firebase.auth.FirebaseAuth;
 
 public class MainActivity extends AppCompatActivity implements OnClickListener {
 
-    @Override
+	@Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
@@ -18,19 +21,19 @@ public class MainActivity extends AppCompatActivity implements OnClickListener {
         Button loginButton = (Button)findViewById(R.id.mlogin);
         loginButton.setOnClickListener(this);
     }
-    
-    public void onClick(View view) {
-        switch (view.getId()) {
-            case R.id.mregister: {
-                Intent intent = new Intent(this,Register.class);
-                startActivity(intent);
-                break;
-            }
-            case R.id.mlogin: {
-                Intent intent = new Intent(this,Login.class);
-                startActivity(intent);
-                break;
-            }
-        }
+
+	public void onClick(View view) {
+		switch (view.getId()) {
+		case R.id.mregister: {
+			Intent intent = new Intent(this, Register.class);
+			startActivity(intent);
+			break;
+		}
+		case R.id.mlogin: {
+			Intent intent = new Intent(this, Login.class);
+			startActivity(intent);
+			break;
+		    }
+	    }
     }
 }

a6f02b830b36f51c33a87aab82cc8918c81f314d
Deniz Sevinc <dsevi104@uottawa.ca>
2020-10-30 16:20:02 -0400

    more ui cleanup

--- ServiceNovigrad/app/src/main/java/com/example/servicenovigrad/Admin.java
+++ ServiceNovigrad/app/src/main/java/com/example/servicenovigrad/Admin.java
@@ -1,5 +1,7 @@
 package com.example.servicenovigrad;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class Admin extends User {
 
@@ -12,4 +14,24 @@ public class Admin extends User {
     protected int getLayoutResourceId() {
         return R.layout.activity_admin;
     }
+
+//    public void onClickAdmin(View view) {
+//        switch (view.getId()) {
+//            case R.id.aAddService: {
+//                Intent intent = new Intent(this, Service.class);
+//                startActivity(intent);
+//                break;
+//            }
+//            case R.id.aEditService: {
+//                Intent intent = new Intent(this, Service.class);
+//                startActivity(intent);
+//                break;
+//            }
+//            case R.id.aRemoveService: {
+//                Intent intent = new Intent(this, Service.class);
+//                startActivity(intent);
+//                break;
+//            }
+//        }
+//    }
 }

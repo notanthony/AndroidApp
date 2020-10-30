1918ca5b4358bd2367b42f331616c2a3cddd3e75
anthony <anthony.andy.zhao@gmail.com>
2020-10-29 20:36:44 -0400

    New Service Object

---
+++ ServiceNovigrad/app/src/main/java/com/example/servicenovigrad/Service.java
@@ -0,0 +1,46 @@
+public class Service { 
+	private String name;
+	private double price;
+	private String[] forms;
+	private String[] documents;
+	
+	public void Service(String name, double price, String[] forms, String[] documents){
+		this.name = name;
+		this.price = price;
+		this.forms = forms;
+		this.documents = documents;
+	}
+	
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	public String getName() {
+		return name;
+	}
+	
+	public void setPrice(double price) {
+		this.price = price;
+	}
+	
+	public double getPrice() {
+		return price;
+	}
+	
+	public String[] getForms() {
+		return forms;
+	}
+	
+	public String[] getDocuments() {
+		return documents;
+	}
+	
+	public void setDocuments(int index, String document) {
+		documents[index] = document;
+	}
+	
+	public void setForms(int index, String document) {
+		forms[index] = forms;
+	}
+	
+}

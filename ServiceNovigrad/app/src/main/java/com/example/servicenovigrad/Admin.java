cc8eac443605a4471ceeee57d9a9919aa3e0bd7b
Deniz Sevinc <dsevi104@uottawa.ca>
2020-10-30 16:54:10 -0400

    more ui changes - user welcome message size change

--- ServiceNovigrad/app/src/main/java/com/example/servicenovigrad/Admin.java
+++ ServiceNovigrad/app/src/main/java/com/example/servicenovigrad/Admin.java
@@ -3,7 +3,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
-public class Admin extends User {
+public class Admin extends User  {
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -15,23 +15,23 @@ public class Admin extends User {
         return R.layout.activity_admin;
     }
 
-//    public void onClickAdmin(View view) {
-//        switch (view.getId()) {
-//            case R.id.aAddService: {
-//                Intent intent = new Intent(this, Service.class);
-//                startActivity(intent);
-//                break;
-//            }
-//            case R.id.aEditService: {
-//                Intent intent = new Intent(this, Service.class);
-//                startActivity(intent);
-//                break;
-//            }
-//            case R.id.aRemoveService: {
-//                Intent intent = new Intent(this, Service.class);
-//                startActivity(intent);
-//                break;
-//            }
-//        }
-//    }
+    public void onClickAdmin(View view) {
+        switch (view.getId()) {
+            case R.id.addServiceButton: {
+                Intent intent = new Intent(this, Service.class);
+                startActivity(intent);
+                break;
+            }
+            case R.id.editServiceButton: {
+                Intent intent = new Intent(this, Service.class);
+                startActivity(intent);
+                break;
+            }
+            case R.id.removeServiceButton: {
+                Intent intent = new Intent(this, Service.class);
+                startActivity(intent);
+                break;
+            }
+        }
+    }
 }

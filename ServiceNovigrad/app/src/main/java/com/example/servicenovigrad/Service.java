c8f160856c30719d7dcf7d8479a030bc7888afb1
anthony <anthony.andy.zhao@gmail.com>
2020-10-31 20:08:35 -0400

    small changes

--- ServiceNovigrad/app/src/main/java/com/example/servicenovigrad/Service.java
+++ ServiceNovigrad/app/src/main/java/com/example/servicenovigrad/Service.java
@@ -3,55 +3,34 @@ package com.example.servicenovigrad;
 import java.util.List;
 
 public class Service {
-	private String _id;
-	private String _name;
-	private double _price;
-	private List<String> _forms;
-	private List<String> _docs;
+	private String id;
+	private String name;
+	private double price;
+	private List<String> forms;
+	private List<String> docs;
 
 	public Service() {
 		super();
 	}
-	public Service(String id, String name, double price, List<String> forms, List<String> docs){
-		_id = id;
-		_name = name;
-		_price = price;
-		_forms = forms;
-		_docs = docs;
+	public Service(String id, String name, double price, List<String> forms, List<String> docs) {
+		this.id = id;
+		this.name = name;
+		this.price = price;
+		this.forms = forms;
+		this.docs = docs;
 	}
-	/*
-	public void setName(String name) {
-		_name = name;
-	}
-
-	public String getName() {
-		return _name;
-	}
-
-	public void setPrice(double price) {
-		_price = price;
-	}
-
-	public double getPrice() {
-		return _price;
-	}
-	public String toString(){
-		return _name;
-	}*/
 
 	public void setId(String id) {
-		_id = id;
+		this.id = id;
 	}
-	public String getId() { return _id; }
-	public void setServiceName(String name) { _name = name; }
-	public String getServiceName() { return _name; }
-	public void setPrice(double price) { _price = price; }
-	public double getPrice() { return _price; }
-	public void setForms(List<String> forms) { _forms = forms; }
-	public List<String> getForms() { return _forms; }
-	//public void setForms(int index, String form) { _forms[index] = form; }
-	public List<String> getDocs() { return _docs; }
-	//public void setDocs(int index, String doc) { _docs[index] = doc; }
+	public String getId() { return this.id; }
+	public void setServiceName(String name) { this.name = name; }
+	public String getServiceName() { return this.name; }
+	public void setPrice(double price) { this.price = price; }
+	public double getPrice() { return this.price; }
+	public void setForms(List<String> forms) { this.forms = forms; }
+	public List<String> getForms() { return this.forms; }
+	public List<String> getDocs() { return this.docs; }
 
 
 

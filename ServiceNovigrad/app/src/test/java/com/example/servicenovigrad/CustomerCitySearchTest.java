535bdfdc328a33e37659153e2e669998be376d40
Deniz Sevinc <dsevi104@uottawa.ca>
2020-12-06 21:26:12 -0500

    fixed search buttons

--- ServiceNovigrad/app/src/test/java/com/example/servicenovigrad/CustomerCitySearchTest.java
+++ ServiceNovigrad/app/src/test/java/com/example/servicenovigrad/CustomerCitySearchTest.java
@@ -1,13 +1,21 @@
 package com.example.servicenovigrad;
 
-import junit.framework.TestCase;
+import android.content.Context;
 
 import org.junit.Test;
-
-import static org.hamcrest.CoreMatchers.is;
+import org.junit.runner.RunWith;
+import org.junit.Test;
+import static org.junit.Assert.*;
+import org.mockito.Mock;
+import org.mockito.runners.MockitoJUnitRunner;
 import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.CoreMatchers.*;
 
-public class CustomerCitySearchTest extends TestCase {
+@RunWith(MockitoJUnitRunner.class)
+public class CustomerCitySearchTest {
+    @Mock
+    Context mMockContext;
+
     @Test
     public void customerCitySearchTest(){
         String testCity = "Ottawa";
--- ServiceNovigrad/app/src/test/java/com/example/servicenovigrad/EmployeeEditHoursTest.java
+++ ServiceNovigrad/app/src/test/java/com/example/servicenovigrad/EmployeeEditHoursTest.java
@@ -7,9 +7,9 @@ import org.junit.Test;
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.MatcherAssert.assertThat;
 
-public class EmployeeEditHoursTest extends TestCase {
+public class EmployeeEditHoursTest {
     @Test
-    public static void employeeEditHourTest(){
+    public void employeeEditHourTest(){
         String hour = "07:00 AM";
         EmployeeEditHours Test = new EmployeeEditHours();
         boolean result = Test.validateBranchHourChange(hour);

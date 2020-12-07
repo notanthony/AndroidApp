f53436a41c97b11fd59554c7fdaa67558a566914
Deniz Sevinc <dsevi104@uottawa.ca>
2020-12-06 22:28:57 -0500

    minor change

--- ServiceNovigrad/app/src/main/java/com/example/servicenovigrad/CustomerCreateServiceRequest.java
+++ ServiceNovigrad/app/src/main/java/com/example/servicenovigrad/CustomerCreateServiceRequest.java
@@ -3,6 +3,7 @@ package com.example.servicenovigrad;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.EditText;
 import android.widget.LinearLayout;
